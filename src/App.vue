20936d651e980607cf739b5acbfee5e22e596ca0
Cloud <theanswer0411@gmail.com>
2024-12-29 15:22:51 +0800

    Add Vue objects

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <!-- <div class="mt-10 text-center container">
+        <button @click="showDialog=true" class="btn btn-success ">Delete Task</button>
+        <button @click="showDia" class="btn btn-danger ">Delete User</button>
+    </div> -->
+    <HelloWorld/>
+    <!-- <MyDialog :show="showDialog" :cancel="cancel" :confirm="confirm" title="Delete a task?" description="Dynamical description" /> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+// import MyDialog from "./components/MyDialog.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      showDialog:false
+    }
+
+  },
+  methods:{
+    cancel() {
+      console.log("cancel");
+      this.showDialog=false;
+    },
+    confirm() {
+      console.log("confirm");
+      this.showDialog=false;
+    },
+    showDia() {
+      console.log("show dialog");
+      this.showDialog=true;
+    }
+  }
+};
+</script>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="HelloWorld">
+    <div class="container">
+      <div class="form row">
+        <p>
+          <button
+            type="button"
+            class="btn btn-primary col row-auto"
+            data-bs-toggle="modal"
+            data-bs-target="#testModal"
+          >
+            Launch demo modal
+          </button>
+        </p>
+      </div>
+    </div>
+
+    <div class="modal fade" id="testModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div class="formg-roup">
+              <label for="email">Your email</label>
+              <input id="email" type="email" class="form-control" required />
+            </div>
+            <div class="formg-roup">
+              <label for="password">Your password</label>
+              <input
+                id="password"
+                type="password"
+                class="form-control"
+                required
+              />
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
